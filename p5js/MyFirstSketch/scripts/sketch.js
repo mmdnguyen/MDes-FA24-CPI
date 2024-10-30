527f3a982ca08ed088f40e901d8a1397ade0bafa
mmdnguyen <mnguyen7@inside.artcenter.edu>
2024-10-29 19:34:53 -0600

    added week 08 folder

---
+++ p5js/MyFirstSketch/scripts/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(255, 0, 0);
+}
+
+// HELPER STUFF:
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
